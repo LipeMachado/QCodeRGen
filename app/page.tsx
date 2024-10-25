80a7836b32e9240eb134026894232887945928a0
LipeMachado <silvalipe765@gmail.com>
2024-10-25 17:14:20 -0300

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import QRCodeGenerator from "./QRCodeGenerator";
 
 export default function Home() {
   return (
     <>
-      
+      <div className="relative min-h-[100vh] h-full flex justify-center items-center">
+        <QRCodeGenerator />
+        <Image
+          src="/glass.png"
+          width={1600}
+          height={1200}
+          alt="glass image"
+          className="fixed top-0 left-0 w-full h-full object-cover z-0 pointer-events-none"
+        />
+      </div>
     </>
   );
 }
