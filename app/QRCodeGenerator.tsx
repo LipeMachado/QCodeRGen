048f9c9b22003a8a154acd7dcd0454250e931b31
LipeMachadoVezos <felipe.machado@vezos.com.br>
2024-11-01 15:20:23 -0300

    Update Changes

--- app/QRCodeGenerator.tsx
+++ app/QRCodeGenerator.tsx
@@ -1,10 +1,26 @@
+"use client"
+
 import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
 
 export default function QRCodeGenerator() {
   return (
     <>
-      <div>
-
+      <div className='relative z-10 mx-6 flex max-w-[1250px] w-full min-h-[700px] h-full '>
+        <Card className='flex-1 flex flex-col w-full h-auto mx-auto bg-[#ecf7ff]/80 backdrop-blur-md shadow-sm border-2 border-white/40 rounded-xl'>
+          <CardHeader>
+            <CardTitle className='text-3xl font-bold text-center text-[#037fff]'>QR Code Generator</CardTitle>
+          </CardHeader>
+          <CardContent className='flex-1'>
+            <div className='h-full flex flex-col md:flex-row gap-8'>
+            </div>            
+          </CardContent>
+        </Card>
       </div>
     </>
   )
